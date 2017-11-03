8230dfea741680f95ed9e20a509e6033c22a8c72
r1ft1 <jamessundaram8@gmail.com>
2017-11-03 10:11:15 +0000

    Add files via upload

---
+++ BoidManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour {
+
+
+	public GameObject[] Boids = new GameObject[5];
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	Vector3 velocity;
+	Rigidbody rb;  
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	public void Move (Vector3 _velocity) {
+		velocity = _velocity;
+	}
+		
+
+	void FixedUpdate() {
+		rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		Rotation ();
+	}
+
+	public void Rotation()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);  //Ray from mouse cursor in direction of camera 
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float camRayLength;
+
+		if (groundPlane.Raycast (camRay, out camRayLength)) {
+			Vector3 point = camRay.GetPoint (camRayLength);
+
+			Debug.DrawLine (camRay.origin, point, Color.red);
+			LookAt (point);
+		}
+		
+	}
+
+	void LookAt(Vector3 lookPoint)
+	{
+		Vector3 heightCorrectedPoint = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);
+		transform.LookAt (heightCorrectedPoint);
+	}
+}
